22bd73eb0afe2ded87e18ab78cd01feaa223d911
Nisrine lachguer <nisrinelachguer37@gmail.com>
2025-12-19 13:50:44 +0100

    CI/CD

--- POV-JAVA/src/main/java/com/example/Point/of/sale/controller/HelloController.java
+++ POV-JAVA/src/main/java/com/example/Point/of/sale/controller/HelloController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping
     public String hello() {
-        return "Hello automatisation with jenkins)";
+        return "Hello automatisation with jenkins Khaoula hihihi)";
     }
 
     @GetMapping("/user")
